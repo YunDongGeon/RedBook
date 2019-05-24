25303baa6a138437392bde131b243e692c29f3fd
YunDongGeon <dbsehdrjs20@gmail.com>
2019-05-24 15:34:17 +0900

    flask

--- redbook.py
+++ redbook.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import send_from_directory
 
 app = Flask(__name__, static_url_path="/templates")
@@ -10,8 +10,8 @@ def serve_page(path):
     return send_from_directory('templates', path)
 
 @app.route("/")
-def hello():
-    return send_from_directory('templates', 'index.html')
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
